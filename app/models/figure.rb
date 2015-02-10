52383207e7c18bae7a88b86ee5f9e9c4ec9b3176
Frank Kotsianas <frankkotsianas@gmail.com>
2015-02-10 00:02:56 -0800

    Figure accepts nested attrs for images.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,7 +1,7 @@
 class Figure < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   belongs_to :figure_category
-  has_many :images, class_name: :figure_image, dependent: :destroy
+  has_many :images, class_name: 'FigureImage', dependent: :destroy
   
   accepts_nested_attributes_for :images, :allow_destroy => true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,28 +15,27 @@ figure_list = [
   #name, category, image
   ['Benjamin Franklin', 
     :historical_figures, 
-    {url: '/images/benjamin_franklin.jpg'}
+    {url: 'benjamin_franklin.jpg'}
   ],
   [
     'Katy Perry',
     :celebrities,
-    {url: '/images/katy_perry.jpg'}
+    {url: 'katy_perry.jpg'}
   ],
   [
     'Leonardo DiCaprio',
     :celebrities,
-    {url: '/images/leonardo_dicaprio.jpg'}
+    {url: 'leonardo_dicaprio.jpg'}
   ],
   [
     'Jonah Hill',
     :celebrities,
-    {url: '/images/jonah_hill.jpg'}
+    {url: 'jonah_hill.jpg'}
   ]
 ]
 
 figures = figure_list.map do |name, category, image_attrs|
   Figure.create(name: name, 
                 figure_category: FigureCategory.find_by(name: category), 
-                # figure_image_attributes: image_attrs)
-                )
+                images_attributes: [image_attrs])
 end
