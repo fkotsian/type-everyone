0d79cf3c4f554b95e9e3d343f99f7d5f90d4c005
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-18 19:56:53 -0700

    Rename play_path to game_path

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,15 +4,15 @@ class VotesController < ApplicationController
       figure_id: vote_params[:figure_id],
       vote_type_id: vote_type.id
     )
-    redirect_to random_path
+    redirect_to game_path
   end
-    
+
   private
-  
+
   def vote_params
     params.require(:vote).permit(:figure_id)
   end
-  
+
   def vote_type
     VoteType.find_by(name: params["commit"].downcase)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-    
+
   resources :figures,       only: %w[index new create show]
   resources :figure_images, only: %w[index new create show]
 
-  get '/play',  to: 'game#type'
+  get '/play',  to: 'game#type', as: 'game'
   post '/votes', to: 'votes#create'
-  
+
   root to: 'game#type'
 end
