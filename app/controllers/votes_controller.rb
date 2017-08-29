ef7f41741fb74585ffaac377d76493d7f612441c
Frank Kotsianas <frankkotsianas@gmail.com>
2017-08-29 12:12:56 -0700

    Flash voter percentage after vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,12 @@ class VotesController < ApplicationController
       figure_id: vote_params[:figure_id],
       vote_type_id: vote_type.id
     )
+    total_votes = Vote.for_figure(vote_params[:figure_id])
+                    .count
+    agreed_votes = Vote.for_figure(vote_params[:figure_id])
+                    .for_type(vote_type.id)
+                    .count
+    flash[:success] = "#{(agreed_votes/(total_votes*1.0)*100).round}% of voters agree with you!"
     redirect_to game_path
   end
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :vote_type
   belongs_to :figure
+
+  scope :for_figure, ->(id) { where(figure_id: id) }
+  scope :for_type,   ->(id) { where(vote_type_id: id) }
 end
