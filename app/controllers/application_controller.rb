9a7b65e1dc6d69b11b56ac72de4bc53c9c7f175e
Frank Kotsianas <frankkotsianas@gmail.com>
2019-05-30 21:18:41 -0700

    after_action -> before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_action do
+  before_action do
     Rails.logger.error "*"*100
     Rails.logger.error "PARAMS"
     Rails.logger.error params
